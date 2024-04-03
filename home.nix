2eaa8c32572f57fd3658af0da044702c79d63474
Timon Schwarz <timon.anmeldung@gmail.com>
2024-04-03 16:32:22 +0200

    feat: Initial commit

---
+++ home.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "schwarz";
+  home.homeDirectory = "/var/home/schwarz";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    pkgs.neovim
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    "hypr/hyprland.conf".source = dotfiles/hypr/hyprland.conf;
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. 
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
